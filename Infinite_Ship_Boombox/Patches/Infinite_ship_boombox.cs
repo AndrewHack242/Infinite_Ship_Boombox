61cc6138d8bc4a6efead62add33fe6689d355ecb
AndrewHack242 <andrew.hack242@gmail.com>
2024-04-22 20:19:51 -0600

    Add project files.

---
+++ Infinite_Ship_Boombox/Patches/Infinite_ship_boombox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace Infinite_Ship_Boombox.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    internal class Infinite_ship_boombox
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void patch_func(ref bool ___isInShipRoom, ref Item ___itemProperties)
+        {
+            if (___isInShipRoom)
+            {
+                ___itemProperties.requiresBattery = false;
+            }
+            else
+            {
+                ___itemProperties.requiresBattery = true;
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patch_func(ref Item ___itemProperties)
+        {
+            ___itemProperties.requiresBattery = true;
+        }
+    }
+}
+ 
---
+++ Infinite_Ship_Boombox/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Infinite_Ship_Boombox
+{
+    [BepInPlugin(mod_GUID, mod_name, mod_version)]
+    public class Infinite_ship_boombox_base : BaseUnityPlugin
+    {
+        private const string mod_GUID = "Hackattack242.Infinite_Ship_Boombox";
+        private const string mod_name = "Infinite_Ship_Boombox";
+        private const string mod_version = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(mod_GUID);
+
+        private static Infinite_ship_boombox_base Instance;
+
+        private ManualLogSource logger;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(mod_name);
+
+            logger.LogInfo("Infinite Ship Boombox has awoken");
+
+            harmony.PatchAll();
+        }
+
+        internal static void LogDebug(string message)
+        {
+            Instance.Log(message, (LogLevel)32);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            Instance.Log(message, (LogLevel)16);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Instance.Log(message, (LogLevel)4);
+        }
+
+        internal static void LogError(string message)
+        {
+            Instance.Log(message, (LogLevel)2);
+        }
+
+        internal static void LogError(Exception ex)
+        {
+            Instance.Log(ex.Message + "\n" + ex.StackTrace, (LogLevel)2);
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            ((Infinite_ship_boombox_base)this).logger.Log(logLevel, (object)message);
+        }
+
+    }
+}
